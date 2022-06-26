758ade297d411f2ac1bec77b7a19f330803f2a16
Ramya Ramakrishnan <106366228+rramakr7@users.noreply.github.com>
2022-06-26 23:09:16 +0530

    Add files via upload

---
+++ SQL/Activities/Activity4.sql
@@ -0,0 +1,9 @@
+REM   Script: SQL - Activity4
+REM   FST - SQL - Activity4
+
+ALTER TABLE salesman ADD grade int;
+
+UPDATE salesman SET grade = 100;
+
+SELECT * FROM salesman;
+
---
+++ SQL/Activities/Activity5.sql
@@ -0,0 +1,9 @@
+REM   Script: SQL - Activity5
+REM   FST - SQL - Activity5
+
+UPDATE salesman SET grade = 200 WHERE salesman_city = 'Rome';
+
+UPDATE salesman SET grade = 300 WHERE salesman_name = 'James Hoog';
+
+UPDATE salesman SET salesman_name = 'Pierre' WHERE salesman_name = 'McLyon';
+
